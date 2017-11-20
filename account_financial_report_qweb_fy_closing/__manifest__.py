2eb4462a2b4101b07c8e290639d11e8d50ed5999
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-11-20 16:07:38 +0100

    [ADD] account_financial_report_qweb_fy_closing: Glue module
    Glue module between account_financial_report_qweb and fiscal year closing

---
+++ account_financial_report_qweb_fy_closing/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Financial reports with fiscal year closing",
+    "version": "10.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://www.tecnativa.con/",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_financial_report_qweb",
+        "account_fiscal_year_closing",
+    ],
+    "auto_install": True,
+}
